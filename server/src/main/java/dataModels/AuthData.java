a63c062378ddf101d6ed5bdfc7a9486122e0bab7
Spencer Clingo <spencerclingo@gmail.com>
2024-02-22 18:44:41 -0700

    Models are now records

--- server/src/main/java/dataModels/AuthData.java
+++ server/src/main/java/dataModels/AuthData.java
@@ -1,27 +1,4 @@
 package dataModels;
 
-public class AuthData {
-    private String authToken;
-    private String username;
-
-    public AuthData(String authToken, String username) {
-        this.authToken=authToken;
-        this.username=username;
-    }
-
-    public String getAuthToken() {
-        return authToken;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken=authToken;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username=username;
-    }
+public record AuthData(String authToken, String username) {
 }
--- server/src/main/java/dataModels/GameData.java
+++ server/src/main/java/dataModels/GameData.java
@@ -3,36 +3,4 @@ package dataModels;
 import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-    public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-        this.gameID=gameID;
-        this.whiteUsername=whiteUsername;
-        this.blackUsername=blackUsername;
-        this.gameName=gameName;
-        this.game=game;
-    }
-
-    @Override
-    public int gameID() {
-        return gameID;
-    }
-
-    @Override
-    public String whiteUsername() {
-        return whiteUsername;
-    }
-
-    @Override
-    public String blackUsername() {
-        return blackUsername;
-    }
-
-    @Override
-    public String gameName() {
-        return gameName;
-    }
-
-    @Override
-    public ChessGame game() {
-        return game;
-    }
 }
